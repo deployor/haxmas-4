731a5e418130bc630dbdc4148388b58f4e537013
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2025-12-16 16:02:22 +0000

    doing pt2

--- my-project/src/db/index.ts
+++ my-project/src/db/index.ts
@@ -1,12 +1,7 @@
-import { Hono } from 'hono'
+import "dotenv/config"
+import { drizzle } from "drizzle-orm/bun-sqlite"
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-export default app
+export const db = drizzle(process.env.DB_FILE_NAME!)
 
 import { createWish, deleteWish, fulfillWish, listWishes } from "./db/queries"
 
